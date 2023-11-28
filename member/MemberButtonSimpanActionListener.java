17a36b456d804843298f85fae1fed8443da1eab6
Syahnan <syhnnzhra@gmail.com>
2023-11-28 11:20:04 +0700

    tugas pertemuan 8

---
+++ member/MemberButtonSimpanActionListener.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package member;
+
+import jenis_member.JenisMember;
+import java.awt.event.*;
+import java.util.UUID;
+import dao.MemberDao;
+/**
+ *
+ * @author syhnn
+ */
+public class MemberButtonSimpanActionListener implements ActionListener {
+    private MemberFrame memberFrame;
+    private MemberDao memberDao;
+    
+    private MemberButtonSimpanActionListener(MemberFrame memberFrame, MemberDao memberDao){
+        this.memberFrame = memberFrame;
+        this.memberDao = memberDao;
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        String nama = this.memberFrame.getNama();
+        if(nama.isEmpty()){
+            this.memberFrame.showAlert("Nama Tidak Boleh Kosong");
+            this.memberDao = memberDao;
+        } else {
+            JenisMember jenisMember = this.memberFrame.getJenisMember();
+            Member member = new Member();
+            member.setId(UUID.randomUUID().toString());
+            member.setNama(nama);
+            member.setJenisMember(jenisMember);
+            
+            this.memberFrame.addMember(member);
+            this.memberDao.insert(member);
+        }
+    }
+}
---
+++ member/MemberFrame.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package member;
+
+import jenis_member.JenisMember;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.util.*;
+import dao.MemberDao;
+import dao.JenisMemberDao;
+/**
+ *
+ * @author syhnn
+ */
+public class MemberFrame extends JFrame{
+    private List<JenisMember> jenisMemberList;
+    private List<Member> memberList;
+    private JTextField textFieldNama;
+    private MemberTableModel tableModel;
+    private JComboBox comboJenis;
+    private MemberDao memberDao;
+    private JenisMemberDao jenisMemberDao;
+    
+    public MemberFrame(MemberDao memberDao, JenisMemberDao jenisMemberDao){
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        this.memberDao = memberDao;
+        this.jenisMemberDao = jenisMemberDao;
+        this.memberList = memberDao.findAll();
+        
+        JLabel lableInput = new JLabel("Nama :");
+        lableInput.setBounds(15, 40, 350, 10);
+        
+        textFieldNama = new JTextField();
+        textFieldNama.setBounds(15, 60, 350, 30);
+        
+        JLabel labelJenis = new JLabel("Jenis Member:");
+        labelJenis.setBounds(15, 100, 350, 10);
+        
+        comboJenis = new JComboBox();
+        comboJenis.setBounds(15, 120, 130, 30);
+        
+        JButton button = new JButton("Simpan");
+        button.setBounds(15, 100, 100, 40);
+        
+        javax.swing.JTable table = new JTable();
+        JScrollPane scrollableTable = new JScrollPane(table);
+        scrollableTable.setBounds(15, 220, 350, 200);
+        
+        tableModel = new MemberTableModel(memberList);
+        table.setModel(tableModel);
+        
+        JenisMemberButtonSimpanActionListener actionListener = new JenisMemberButtonSimpanActionListener(this, memberDao);
+        
+        button.addActionListener(actionListener);
+        
+        this.add(button);
+        this.add(textFieldNama);
+        this.add(lableInput);
+        this.add(labelJenis);
+        this.add(comboJenis);
+        this.add(comboJenis);
+        this.add(scrollableTable);
+        
+        this.setSize(400, 500);
+        this.setLayout(null);
+    }
+    
+    public void populateComboJenis() {
+        this.jenisMemberList = this.jenisMemberDao.findAll();
+        comboJenis.removeAllItems();
+        for(JenisMember jenisMember : this.jenisMemberList){
+            comboJenis.addItem(jenisMember.getNama());
+        }
+    }
+    
+    public String getNama() {
+        return textFieldNama.getText();
+    }
+    
+    public JenisMember getJenisMember() {
+        return jenisMemberList.get(comboJenis.getSelectedIndex());
+    }
+    
+    public void addMember(Member member){
+        tableModel.add(member);
+        textFieldNama.setText("");
+    }
+    
+    public void showAlert(String message){
+        JOptionPane.showMessageDialog(this, message);
+    }
+}
